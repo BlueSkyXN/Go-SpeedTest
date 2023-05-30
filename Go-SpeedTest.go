a8ea11d4f457cc38a335a02a71333d7548c0149d
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-05-30 22:31:13 +0800

    0.2

--- Go-SpeedTest.go
+++ Go-SpeedTest.go
@@ -1,19 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"os/signal"
 	"sync/atomic"
+	"syscall"
+	"time"
 
-	"gioui.org/app"
-	"gioui.org/io/system"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/unit"
-	"gioui.org/widget"
-	"gioui.org/widget/material"
+	"github.com/inancgumus/screen"
 	"gopkg.in/ini.v1"
 )
 
@@ -23,15 +21,6 @@ func (c *AtomicCounter) Write(p []byte) (n int, err error) {
 	n = len(p)
 	atomic.AddInt64((*int64)(c), int64(n))
 	return
-}
-
-func averageSpeed(ringBuffer []float64, currentIndex, seconds int) float64 {
-	start := (currentIndex + 1 + 60 - seconds) % 60
-	total := 0.0
-	for i := 0; i < seconds; i++ {
-		total += ringBuffer[(start+i)%60]
-	}
-	return total / float64(seconds)
 }
 
 func main() {
@@ -48,82 +37,67 @@ func main() {
 	ringBuffer := make([]float64, 60)
 	index := 0
 
-	// Create Gio app
+	ctx, cancel := context.WithCancel(context.Background())
+
 	go func() {
-		w := app.NewWindow(app.Size(unit.Dp(400), unit.Dp(200)))
-		th := material.NewTheme()
-
-		var mbpsLabel, avg3sLabel, avg10sLabel, avg60sLabel widget.Label
-		var ops op.Ops
-
 		for {
-			e := <-w.Events()
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				time.Sleep(time.Second)
 
-			switch e := e.(type) {
-			case system.DestroyEvent:
-				os.Exit(0)
-			case system.FrameEvent:
-				// Update data
 				Mbps := float64(atomic.LoadInt64((*int64)(counter)) * 8 / 1024 / 1024)
 				ringBuffer[index] = Mbps
+
 				avg3s := averageSpeed(ringBuffer, index, 3)
 				avg10s := averageSpeed(ringBuffer, index, 10)
 				avg60s := averageSpeed(ringBuffer, index, 60)
 
-				// Clear the operations
-				ops.Reset()
+				screen.Clear()
+				screen.MoveTopLeft()
 
-				// Update labels
-				mbpsLabel.Text = fmt.Sprintf("Current Speed: %.2f Mbps", Mbps)
-				avg3sLabel.Text = fmt.Sprintf("3s Average: %.2f Mbps", avg3s)
-				avg10sLabel.Text = fmt.Sprintf("10s Average: %.2f Mbps", avg10s)
-				avg60sLabel.Text = fmt.Sprintf("60s Average: %.2f Mbps", avg60s)
+				fmt.Printf("| Time      | Current Speed | 3s Average | 10s Average | 60s Average |\n")
+				fmt.Printf("|-----------|---------------|------------|-------------|-------------|\n")
+				fmt.Printf("| %s | %-13.2f | %-10.2f | %-11.2f | %-11.2f |\n", time.Now().Format("15:04:05"), Mbps, avg3s, avg10s, avg60s)
 
-				// Build the layout
-				layout.Flex{Axis: layout.Vertical}.Layout(&ops,
-					layout.Rigid(func() {
-						layout.Center.Layout(&ops, func() {
-							material.H1(th, mbpsLabel.Text).Layout(&ops)
-						})
-					}),
-					layout.Rigid(func() {
-						layout.Center.Layout(&ops, func() {
-							material.Body1(th, avg3sLabel.Text).Layout(&ops)
-						})
-					}),
-					layout.Rigid(func() {
-						layout.Center.Layout(&ops, func() {
-							material.Body1(th, avg10sLabel.Text).Layout(&ops)
-						})
-					}),
-					layout.Rigid(func() {
-						layout.Center.Layout(&ops, func() {
-							material.Body1(th, avg60sLabel.Text).Layout(&ops)
-						})
-					}),
-				)
-
-				// Draw the operations
-				e.Frame(&ops)
+				atomic.StoreInt64((*int64)(counter), 0)
+				index = (index + 1) % 60
 			}
 		}
 	}()
 
-	// Moved http request into the goroutine.
 	go func() {
-		res, err := http.Get(url)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		defer res.Body.Close()
-
-		_, err = io.Copy(io.Discard, io.TeeReader(res.Body, counter))
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+		// 创建一个接收信号的 channel
+		c := make(chan os.Signal, 1)
+		// 监听所有信号
+		signal.Notify(c)
+		// 循环遍历 channel，进行处理
+		for s := range c {
+			if s == syscall.SIGINT {
+				fmt.Println("Caught signal SIGINT, stop downloading...")
+				// Call the cancel function to stop the download
+				cancel()
+				return
+			}
 		}
 	}()
 
-	app.Main()
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	_, _ = io.Copy(io.Discard, io.TeeReader(res.Body, counter))
+}
+
+func averageSpeed(ringBuffer []float64, currentIndex, seconds int) float64 {
+	start := (currentIndex + 1 + 60 - seconds) % 60
+	total := 0.0
+	for i := 0; i < seconds; i++ {
+		total += ringBuffer[(start+i)%60]
+	}
+	return total / float64(seconds)
 }
--- go.mod
+++ go.mod
@@ -2,19 +2,15 @@ module github.com/BlueSkyXN/Go-SpeedTest
 
 go 1.20
 
+require gopkg.in/ini.v1 v1.67.0
+
 require (
-	gioui.org v0.0.0-20230526230622-e3ef98dda382
-	gopkg.in/ini.v1 v1.67.0
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/term v0.8.0 // indirect
 )
 
 require (
-	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
-	gioui.org/shader v1.0.6 // indirect
-	github.com/go-text/typesetting v0.0.0-20230413204129-b4f0492bf7ae // indirect
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3
 	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/exp v0.0.0-20221012211006-4de253d81b95 // indirect
-	golang.org/x/exp/shiny v0.0.0-20220827204233-334a2380cb91 // indirect
-	golang.org/x/image v0.5.0 // indirect
-	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
-	golang.org/x/text v0.7.0 // indirect
 )
