91f7e10abe2f4b78cbe51e9878c824bb8e1f018b
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-05-30 20:23:30 +0800

    0.0.3

--- Go-SpeedTest.go
+++ Go-SpeedTest.go
@@ -3,80 +3,52 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
-	"os/exec"
-	"runtime"
 	"sync/atomic"
 	"time"
 
 	"gopkg.in/ini.v1"
 )
 
-type AtomicCounter struct {
-	val int64
-}
-
-func (c *AtomicCounter) Add(n int64) {
-	atomic.AddInt64(&c.val, n)
-}
-
-func (c *AtomicCounter) Value() int64 {
-	return atomic.LoadInt64(&c.val)
-}
+type AtomicCounter int64
 
 func (c *AtomicCounter) Write(p []byte) (n int, err error) {
-	c.Add(int64(len(p)))
-	return len(p), nil
-}
-
-func clear() {
-	if runtime.GOOS == "windows" {
-		cmd := exec.Command("cmd", "/c", "cls")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	} else {
-		cmd := exec.Command("clear")
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
+	n = len(p)
+	atomic.AddInt64((*int64)(c), int64(n))
+	return
 }
 
 func main() {
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
-		log.Fatalf("Fail to read file: %v", err)
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
 	}
 
 	url := cfg.Section("").Key("url").String()
 
-	counter := &AtomicCounter{}
+	counter := new(AtomicCounter)
 
 	go func() {
 		for {
-			clear()
-			fmt.Printf("%-10s %-8s %-8s\n", "Time", "MBps", "Mbps")
-			bytesPerSecond := counter.Value()
-			fmt.Printf("%s %-8.2f %-8.2f\n", time.Now().Format("15:04:05"), float64(bytesPerSecond)/(1024*1024), float64(bytesPerSecond)*8/(1000*1000))
-			counter.Add(-counter.Value()) // Reset counter
-			time.Sleep(1 * time.Second)
+			time.Sleep(time.Second)
+			MBps := float64(atomic.LoadInt64((*int64)(counter)) / 1024 / 1024)
+			Mbps := MBps * 8
+			fmt.Printf("[%s]  Speed: %-7.2f MB/s | %-7.2f Mbps\n", time.Now().Format("15:04:05"), MBps, Mbps)
+			atomic.StoreInt64((*int64)(counter), 0)
 		}
 	}()
 
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	_, _ = io.Copy(io.Discard, io.TeeReader(res.Body, counter))
+
 	for {
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		mw := io.MultiWriter(counter, io.Discard)
-
-		_, err = io.Copy(mw, resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		resp.Body.Close()
 	}
 }
