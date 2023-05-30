2180bf2ba35671c48389e68982b73ef027c47914
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-05-30 20:25:17 +0800

    0.0.4

--- Go-SpeedTest.go
+++ Go-SpeedTest.go
@@ -30,13 +30,26 @@ func main() {
 
 	counter := new(AtomicCounter)
 
+	ringBuffer := make([]float64, 60)
+	index := 0
+
 	go func() {
 		for {
 			time.Sleep(time.Second)
-			MBps := float64(atomic.LoadInt64((*int64)(counter)) / 1024 / 1024)
-			Mbps := MBps * 8
-			fmt.Printf("[%s]  Speed: %-7.2f MB/s | %-7.2f Mbps\n", time.Now().Format("15:04:05"), MBps, Mbps)
+
+			Mbps := float64(atomic.LoadInt64((*int64)(counter)) * 8 / 1024 / 1024)
+			ringBuffer[index] = Mbps
+
+			avg3s := averageSpeed(ringBuffer, index, 3)
+			avg10s := averageSpeed(ringBuffer, index, 10)
+			avg60s := averageSpeed(ringBuffer, index, 60)
+
+			fmt.Printf("| Time      | Current Speed | 3s Average | 10s Average | 60s Average |\n")
+			fmt.Printf("|-----------|---------------|------------|-------------|-------------|\n")
+			fmt.Printf("| %s | %-13.2f | %-10.2f | %-11.2f | %-11.2f |\n", time.Now().Format("15:04:05"), Mbps, avg3s, avg10s, avg60s)
+
 			atomic.StoreInt64((*int64)(counter), 0)
+			index = (index + 1) % 60
 		}
 	}()
 
@@ -51,4 +64,13 @@ func main() {
 
 	for {
 	}
+}
+
+func averageSpeed(ringBuffer []float64, currentIndex, seconds int) float64 {
+	start := (currentIndex + 1 + 60 - seconds) % 60
+	total := 0.0
+	for i := 0; i < seconds; i++ {
+		total += ringBuffer[(start+i)%60]
+	}
+	return total / float64(seconds)
 }
