04cb7be14de7752e6e0be1bbc11df93de41504e3
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-05-31 22:46:13 +0800

    0.5.2

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"syscall"
 	"time"
@@ -21,12 +22,17 @@ import (
 
 type AtomicCounter int64
 
-type requestInfo struct {
-	protocol   string
-	hostDomain string
-	ip         string
-	port       string
-	path       string
+type ConnectionInfo struct {
+	Index       int
+	Protocol    string
+	Domain      string
+	IP          string
+	Port        string
+	Path        string
+	Speed       float64
+	RingBuffer  []float64
+	CurrentRing int
+	Counter     *AtomicCounter // Add this line
 }
 
 func (c *AtomicCounter) Write(p []byte) (n int, err error) {
@@ -36,6 +42,8 @@ func (c *AtomicCounter) Write(p []byte) (n int, err error) {
 }
 
 func main() {
+	// 这是你希望同时进行的连接数量
+	numConnections := 8
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
 		fmt.Printf("Fail to read file: %v", err)
@@ -43,184 +51,181 @@ func main() {
 	}
 
 	// Load configurations
-	base_url := cfg.Section("url").Key("base_url").String()
-	disable_ssl_verification := cfg.Section("url").Key("disable_ssl_verification").MustBool()
-	ssl_domain := cfg.Section("url").Key("ssl_domain").String()
-	host_domain := cfg.Section("url").Key("host_domain").String()
-	lock_ip := cfg.Section("url").Key("lock_ip").String()
-	lock_port := cfg.Section("url").Key("lock_port").String()
+	baseURL := cfg.Section("url").Key("base_url").String()
+	disableSSLVerification := cfg.Section("url").Key("disable_ssl_verification").MustBool()
+	sslDomain := cfg.Section("url").Key("ssl_domain").String()
+	hostDomain := cfg.Section("url").Key("host_domain").String()
+	lockIP := cfg.Section("url").Key("lock_ip").String()
+	lockPort := cfg.Section("url").Key("lock_port").String()
 
-	if host_domain == "" {
-		u, err := url.Parse(base_url)
+	if hostDomain == "" {
+		u, err := url.Parse(baseURL)
 		if err != nil {
 			fmt.Printf("Failed to parse base URL: %v\n", err)
 			os.Exit(1)
 		}
-		host_domain = u.Host
+		hostDomain = u.Host
 	}
 
-	if lock_port == "" {
-		if strings.HasPrefix(base_url, "https://") {
-			lock_port = "443"
+	if lockPort == "" {
+		if strings.HasPrefix(baseURL, "https://") {
+			lockPort = "443"
 		} else {
-			lock_port = "80"
+			lockPort = "80"
 		}
 	}
 
-	if lock_port == "" {
-		if strings.HasPrefix(base_url, "https://") {
-			lock_port = "443"
-		} else {
-			lock_port = "80"
-		}
-	}
+	// 创建一个新的WaitGroup来等待所有的goroutine完成
+	wg := &sync.WaitGroup{}
+	wg.Add(numConnections)
 
-	// Custom DialContext
-	dialContext := (&net.Dialer{}).DialContext
-	transport := &http.Transport{
-		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-			host, _, err := net.SplitHostPort(addr)
-			if err != nil {
-				return nil, err
-			}
+	// 创建一个连接信息的切片来保存每个连接的信息
+	connInfos := make([]ConnectionInfo, numConnections)
 
-			if host == host_domain {
-				if lock_ip != "" {
-					addr = lock_ip + ":" + lock_port
-				} else {
-					// Resolve DNS and use the first IP
-					ips, err := net.LookupIP(host)
+	// 创建一个新的Context，我们可以取消它来停止所有的goroutine
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// 创建一个新的信号通道来监听SIGINT信号
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT)
+
+	go func() {
+		<-sigChan
+		fmt.Println("Caught signal SIGINT, stop downloading...")
+		cancel()
+	}()
+
+	for i := 0; i < numConnections; i++ {
+		// 在新的goroutine中处理每个连接
+		go func(index int) {
+			defer wg.Done()
+
+			// 创建一个新的AtomicCounter和ringBuffer来追踪这个连接的速度
+			counter := new(AtomicCounter)
+			ringBuffer := make([]float64, 60)
+
+			// Custom DialContext
+			dialContext := (&net.Dialer{}).DialContext
+			transport := &http.Transport{
+				DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+					host, _, err := net.SplitHostPort(addr)
 					if err != nil {
 						return nil, err
 					}
-					if len(ips) == 0 {
-						return nil, fmt.Errorf("no IPs found for host: %s", host)
+
+					if host == hostDomain {
+						if lockIP != "" {
+							addr = lockIP + ":" + lockPort
+						} else {
+							// Resolve DNS and use the first IP
+							ips, err := net.LookupIP(host)
+							if err != nil {
+								return nil, err
+							}
+							if len(ips) == 0 {
+								return nil, fmt.Errorf("no IPs found for host: %s", host)
+							}
+							addr = ips[0].String() + ":" + lockPort
+						}
 					}
-					addr = ips[0].String() + ":" + lock_port
-				}
+					return dialContext(ctx, network, addr)
+				},
 			}
-			return dialContext(ctx, network, addr)
-		},
-	}
 
-	// If set to true, SSL certificate verification will be disabled
-	if disable_ssl_verification {
-		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-	}
+			// If set to true, SSL certificate verification will be disabled
+			if disableSSLVerification {
+				transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+			}
 
-	client := &http.Client{
-		Transport: transport,
-	}
+			client := &http.Client{
+				Transport: transport,
+			}
 
-	req, err := http.NewRequest("GET", base_url, nil)
-	if err != nil {
-		fmt.Printf("Failed to create request: %v\n", err)
-		os.Exit(1)
-	}
+			req, err := http.NewRequest("GET", baseURL, nil)
+			if err != nil {
+				fmt.Printf("Failed to create request: %v\n", err)
+				os.Exit(1)
+			}
 
-	// If the server requires the Host field, set the Host field of the request header
-	if ssl_domain != "" {
-		req.Host = ssl_domain
-	}
+			// If the server requires the Host field, set the Host field of the request header
+			if sslDomain != "" {
+				req.Host = sslDomain
+			}
 
-	counter := new(AtomicCounter)
-	ringBuffer := make([]float64, 60)
-	index := 0
-	client = &http.Client{
-		Transport: transport,
-	}
+			res, err := client.Do(req)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			defer res.Body.Close()
 
-	req, err = http.NewRequest("GET", base_url, nil)
-	if err != nil {
-		fmt.Printf("Failed to create request: %v\n", err)
-		os.Exit(1)
-	}
+			_, _ = io.Copy(io.Discard, io.TeeReader(res.Body, counter))
 
-	// If the server requires the Host field, set the Host field of the request header
-	if ssl_domain != "" {
-		req.Host = ssl_domain
-	}
-	// Save request info for later display
-	reqInfo := requestInfo{
-		protocol:   req.URL.Scheme,
-		hostDomain: req.Host,
-		ip:         lock_ip,
-		port:       lock_port,
-		path:       req.URL.Path,
-	}
+			// Save connection info for later display
 
-	counter = new(AtomicCounter)
-	ringBuffer = make([]float64, 60)
-	index = 0
-
-	ctx, cancel := context.WithCancel(context.Background())
+			connInfos[index] = ConnectionInfo{
+				Index:       index,
+				Protocol:    req.URL.Scheme,
+				Domain:      req.Host,
+				IP:          lockIP,
+				Port:        lockPort,
+				Path:        req.URL.Path,
+				Speed:       float64(atomic.LoadInt64((*int64)(counter))) / (1024 * 1024),
+				RingBuffer:  ringBuffer,
+				CurrentRing: 0,
+				Counter:     counter, // Add this line
+			}
+			atomic.StoreInt64((*int64)(counter), 0)
+		}(i)
+	}
 
 	go func() {
+		// 在新的 goroutine 中定期打印速度信息
 		for {
 			select {
 			case <-ctx.Done():
 				return
 			default:
 				time.Sleep(time.Second)
-
-				Mbps := float64(atomic.LoadInt64((*int64)(counter)) * 8 / 1024 / 1024)
-				ringBuffer[index] = Mbps
-
-				avg3s := averageSpeed(ringBuffer, index, 3)
-				avg10s := averageSpeed(ringBuffer, index, 10)
-				avg60s := averageSpeed(ringBuffer, index, 60)
-
 				screen.Clear()
 				screen.MoveTopLeft()
+				fmt.Printf("|-----------|---------------|------------|-------------|-------------|\n")
+				fmt.Printf("|  Conn#    | Current Speed | 3s Average | 10s Average | 60s Average |\n")
+				fmt.Printf("|-----------|---------------|------------|-------------|-------------|\n")
+				totalSpeed := 0.0
+				for i, connInfo := range connInfos {
+					totalSpeed += connInfo.Speed
+					avg3s := averageSpeed(connInfo.RingBuffer, connInfo.CurrentRing, 3)
+					avg10s := averageSpeed(connInfo.RingBuffer, connInfo.CurrentRing, 10)
+					avg60s := averageSpeed(connInfo.RingBuffer, connInfo.CurrentRing, 60)
+					fmt.Printf("|  %d  | %-13.2f | %-10.2f | %-11.2f | %-11.2f |\n", i+1, connInfo.Speed, avg3s, avg10s, avg60s)
+
+					connInfos[i].Speed = float64(atomic.LoadInt64((*int64)(connInfo.Counter))) / (1024 * 1024)
+					connInfos[i].RingBuffer[connInfos[i].CurrentRing] = connInfos[i].Speed
+					connInfos[i].CurrentRing = (connInfos[i].CurrentRing + 1) % len(connInfos[i].RingBuffer)
+					atomic.StoreInt64((*int64)(connInfo.Counter), 0)
+				}
 
 				fmt.Printf("|-----------|---------------|------------|-------------|-------------|\n")
-				fmt.Printf("|    Time   | Current Speed | 3s Average | 10s Average | 60s Average |\n")
-				fmt.Printf("|-----------|---------------|------------|-------------|-------------|\n")
-				fmt.Printf("|  %s | %-13.2f | %-10.2f | %-11.2f | %-11.2f |\n", time.Now().Format("15:04:05"), Mbps, avg3s, avg10s, avg60s)
-				fmt.Printf("|-----------|---------------|------------|-------------|-------------|\n")
-				fmt.Printf("\nRequest Info:\n")
-				fmt.Printf("Protocol: %s\n", reqInfo.protocol)
-				fmt.Printf("Host-Domain: %s\n", reqInfo.hostDomain)
-				fmt.Printf("IP: %s\n", reqInfo.ip)
-				fmt.Printf("Port: %s\n", reqInfo.port)
-				fmt.Printf("Path: %s\n", reqInfo.path)
-
-				atomic.StoreInt64((*int64)(counter), 0)
-				index = (index + 1) % 60
+				fmt.Printf("Total Speed: %.2f MB/s\n", totalSpeed)
 			}
 		}
 	}()
 
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c)
-		for s := range c {
-			if s == syscall.SIGINT {
-				fmt.Println("Caught signal SIGINT, stop downloading...")
-				cancel()
-				return
-			}
-		}
-	}()
-
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer res.Body.Close()
-
-	_, _ = io.Copy(io.Discard, io.TeeReader(res.Body, counter))
-
-	// Prevent the main function from exiting before the download is complete
-	<-ctx.Done()
+	wg.Wait()
 }
 
-func averageSpeed(ringBuffer []float64, currentIndex, seconds int) float64 {
-	start := (currentIndex + 1 + 60 - seconds) % 60
+func averageSpeed(ringBuffer []float64, currentRing, period int) float64 {
+	count := 0
 	total := 0.0
-	for i := 0; i < seconds; i++ {
-		total += ringBuffer[(start+i)%60]
+	for i := currentRing - period; i < currentRing; i++ {
+		index := i
+		if index < 0 {
+			index += len(ringBuffer)
+		}
+		total += ringBuffer[index]
+		count++
 	}
-	return total / float64(seconds)
+	return total / float64(count)
 }
