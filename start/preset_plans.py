9c95bbd6e4cd2d1372813f49fc5739a77c90bf29
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-06-01 16:09:27 +0800

    1.3.0

--- start/preset_plans.py
+++ start/preset_plans.py
@@ -16,14 +16,56 @@ plans = [
     },
     {
         'url': {
-            'base_url': 'https://speed.cloudflare.com/__down?bytes=400000000',
-            'ssl_domain': 'speed.cloudflare.com',
-            'host_domain': 'speed.cloudflare.com',
-            'lock_ip': '104.27.200.70',
+            'base_url': 'https://download.parallels.com/desktop/v17/17.1.1-51537/ParallelsDesktop-17.1.1-51537.dmg',
+            'ssl_domain': 'download.parallels.com',
+            'host_domain': 'download.parallels.com',
         },
         'Speed': {
-            'connections': '10',
-            'test_duration': '70',
+        }
+    },
+    {
+        'url': {
+            'base_url': 'https://cloudflare.cdn.openbsd.org/pub/OpenBSD/7.3/alpha/install73.iso',
+            'ssl_domain': 'cloudflare.cdn.openbsd.org',
+            'host_domain': 'cloudflare.cdn.openbsd.org',
+        },
+        'Speed': {
+        }
+    },
+    {
+        'url': {
+            'base_url': 'https://cdn.openbsd.org/pub/OpenBSD/7.3/amd64/install73.iso',
+            'ssl_domain': 'cdn.openbsd.org',
+            'host_domain': 'cdn.openbsd.org',
+        },
+        'Speed': {
+        }
+    },
+    {
+        'url': {
+            'base_url': 'https://repo.steampowered.com/arch/valveaur/mesa-aco-git-debug-20.1.0_devel.20200404.ffc7574ff73-7-x86_64.pkg.tar.xz',
+            'ssl_domain': 'repo.steampowered.com',
+            'host_domain': 'repo.steampowered.com',
+        },
+        'Speed': {
+        }
+    },
+    {
+        'url': {
+            'base_url': 'https://cdn.cloudflare.steamstatic.com/steam/apps/256843155/movie_max.mp4',
+            'ssl_domain': 'cdn.cloudflare.steamstatic.com',
+            'host_domain': 'cdn.cloudflare.steamstatic.com',
+        },
+        'Speed': {
+        }
+    },
+    {
+        'url': {
+            'base_url': 'https://speedtest.poorhub.pro/cf.7z',
+            'ssl_domain': 'speedtest.poorhub.pro',
+            'host_domain': 'speedtest.poorhub.pro',
+        },
+        'Speed': {
         }
     },
     # ... 更多的计划 ...
